0980eee46bc2ac1ba34284fe3817ed5c6d1871f5
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-06-26 23:58:22 +0600

    carousel banner added

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import img1 from '../../assets/banner/banner1.png';
+import img2 from '../../assets/banner/banner2.png';
+import img3 from '../../assets/banner/banner3.png';
+
+function Banner() {
+  return (
+    <Swiper
+      modules={[Autoplay]}
+      spaceBetween={30}
+      slidesPerView={2}
+      loop={true}
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+    >
+      <SwiperSlide>
+        <img src={img1} alt="Banner 1" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img2} alt="Banner 2" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img3} alt="Banner 3" />
+      </SwiperSlide>
+    </Swiper>
+  );
+}
+
+export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 function Home() {
   return (
-    <div>
-      
+    <div className='my-7'>
+      <Banner></Banner>
     </div>
   );
 }
