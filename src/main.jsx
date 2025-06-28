45385d3b977b87458b273c454fa5c1fb762e8984
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-06-28 15:25:00 +0600

    use animation on scroll in service card section

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,10 @@ import { router } from './router/router.jsx';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 
-AOS.init();
+AOS.init({
+  duration: 1000,
+  once: false,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/Home/services/ServiceCard.jsx
+++ src/pages/Home/services/ServiceCard.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from "aos";
 
 const ServiceCard = ({ service }) => {
+  useEffect(() => {
+      AOS.refresh();
+    }, []);
     const {Icon, title, description} = service
   return (
-    <div className="bg-white shadow-md rounded-2xl p-6 border hover:shadow-lg transition-all duration-300 text-center text-black hover:bg-lime-300">
+    <div data-aos="fade-up" className="bg-white shadow-md rounded-2xl p-6 border hover:shadow-lg transition-all duration-300 text-center text-black hover:bg-lime-300">
       <div className="text-4xl mb-4 text-lime-700 flex justify-center">
         <Icon />
       </div>
